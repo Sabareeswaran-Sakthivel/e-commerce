a82a1e1c1a9deb9dd7dfccde6ede7dccc2d8e48b
Sabareeswaran S <sabareeswaran.s@freightify.com>
2024-10-04 01:33:07 +0530

    custom message added

---
+++ constants/custom_message.go
@@ -0,0 +1,7 @@
+package constants
+
+const (
+	MSG_NOT_FOUND_ERR      = "Not Found Error"
+	DATE_PARSE_ERR_MESSAGE = "Error parsing start date: %s"
+	INVALID_DATE_RANGE     = "Invalid Date Range"
+)
